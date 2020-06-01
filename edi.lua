8ac92b371724bc2504b9d55dca90f60ae79f1d8c
Aririi <fakemail@email.com>
2020-06-01 11:48:01 -0700

    initial file

---
+++ edi.lua
@@ -0,0 +1,19 @@
+--initial declarations
+local component = require("component")
+local event = require("event")
+local sides = require("sides")
+local alarm = component.os_alarm
+local redstone = component.redstone
+local gpu = component.gpu
+
+
+function redstoneCheck()
+  local signal = redstone.getInput(sides.top)
+  if(signal > 1) then
+    gpu.setBackground(0xFE6B73)
+    alarm.activate()
+  else
+    alarm.deactivate()
+    gpu.setBackground(0x000000)
+  end
+end
